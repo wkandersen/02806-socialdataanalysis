f7eef70a472e51a538294b989ae2dbf862c420b3
Eske Haack <eskehaack@gmail.com>
2026-05-03 15:14:39 +0200

    added more plots

--- project3/map_plots.py
+++ project3/map_plots.py
@@ -20,7 +20,7 @@ geojson = {
     ],
 }
 
-df = pd.read_csv("./data/sociodata.csv")
+df = pd.read_csv("./project3/data/sociodata.csv")
 name_fixes = {
     "MONTCLAIRE": "MONTCLARE",
     "WASHINGTON HEIGHT": "WASHINGTON HEIGHTS",
@@ -32,19 +32,32 @@ df.head()
 
 plot_data = df.copy()
 
-fig = go.Figure(
-    data=px.choropleth_map(
-        plot_data,
-        geojson=geojson,
-        locations="COMMUNITY AREA NAME",
-        color="PER CAPITA INCOME ",
-        featureidkey="properties.community",
-        color_continuous_scale="Magma",
-        map_style="carto-positron",
-        zoom=9,
-        center={"lat": 41.8781, "lon": -87.6298},
-        opacity=0.6,
+
+def plot_income_choropleth(col: str, filename: str):
+    fig = go.Figure(
+        data=px.choropleth_map(
+            plot_data,
+            geojson=geojson,
+            locations="COMMUNITY AREA NAME",
+            color=col,
+            featureidkey="properties.community",
+            color_continuous_scale="Magma",
+            map_style="carto-positron",
+            zoom=9,
+            center={"lat": 41.8781, "lon": -87.6298},
+            opacity=0.6,
+        )
     )
-)
-fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-fig.write_html("../docs/figures/income_choropleth.html", include_plotlyjs="cdn")
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    fig.write_html(f"./docs/figures/{filename}", include_plotlyjs="cdn")
+
+
+if __name__ == "__main__":
+    plot_income_choropleth("HARDSHIP INDEX", "hardship_choropleth.html")
+    plot_income_choropleth("PER CAPITA INCOME ", "income_choropleth.html")
+    plot_income_choropleth(
+        "PERCENT HOUSEHOLDS BELOW POVERTY", "poverty_choropleth.html"
+    )
+    plot_income_choropleth(
+        "PERCENT AGED 16+ UNEMPLOYED", "unemployment_choropleth.html"
+    )
